216fa673367f6b7c361c396206a33375ca86f629
jayeshr28 <jayeshrathore2810@gmail.com>
2023-06-21 17:26:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Task1 from './components/Task1';
+
+function App() {
+  const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff'];
+  return (
+    <>
+    
+<div>
+<Task1 heading='Responsive Paragraph Word Counter '/>
+</div>
+
+    </>
+    
+  );
+}
+
+export default App;
+
